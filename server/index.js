de9b4d95bc8b75e2a9a0b7adc19c18d1fb2b2914
Yago <yago.lm@samsung.com>
2022-11-30 00:42:34 -0300

    register full

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import dotenv from "dotenv"
 import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
+import { register } from "./controllers/auth.js"
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url)
@@ -34,6 +35,10 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage })
+
+
+/* Routes with files */
+app.post("/auth/register", upload.single("picture"), register)
 
 /* Mongoose Setup */
 const PORT = process.env.PORT || 6001
