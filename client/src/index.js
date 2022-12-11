4d4b604b60c9bea6b3928fce2dda1a9099cdeeec
Yago <yago.lm@samsung.com>
2022-12-10 23:42:43 -0300

    theme configuation and setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import authReducer from "./state"
+import authReducer from "./state";
+// eslint-disable-next-line
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import {
@@ -19,9 +20,8 @@ import {
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from 'redux-persist/integration/react';
 
-
-
 const persistConfig = { key: "root", storage, version: 1};
+// eslint-disable-next-line
 const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistReducer,
@@ -32,7 +32,6 @@ const store = configureStore({
       },
     }),
 });
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
